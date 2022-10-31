d91fd04557184044fbb244c7aac113dfad78fef4
NehaBajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2022-10-31 21:30:31 +0530

    css

--- app.js
+++ app.js
@@ -4,23 +4,27 @@ var checkButton = document.querySelector("#check-button");
 var errorMsg = document.querySelector("#error-message");
 var noOfNotes = document.querySelector(".no-of-notes");
 var availableNotes = [2000,500,100,20,10,5,1];
-checkButton.addEventListener("click",function validateBillAndCashPaid(){
+function validateBillAndCashPaid(){
     errorMsg.style.display = "none";
     if(bill.value > 0)
     {
-        if(cashPaid.value >= bill.value)
+        if(cashPaid.value <  bill.value)
         {
-        var amountToBeReturned = cashPaid.value-bill.value;
-        calculateChange(amountToBeReturned);
+            showMsg("The cash provided should atleast be equal to the bill amount");
+        }
+        else if(cashPaid.value===bill.value){
+            showMsg("No need to give any change");
         }
         else{
-         showMsg("The cash provided should atleast be equal to the bill amount");
+       var amountToBeReturned = cashPaid.value-bill.value;
+        calculateChange(amountToBeReturned);
+
         }
     }  
     else{
         showMsg("Invalid Bill Amount");
     }
-});
+};
 function showMsg(message){
     
     errorMsg.style.display = "block";
@@ -28,11 +32,12 @@ function showMsg(message){
 }
  function calculateChange(amountToBeReturned)
  {
-    for(let i=0;i < availableNotes.length; i++)
+    for(let i = 0; i < availableNotes.length; i++)
     {
-        var numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        var numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned=amountToBeReturned % availableNotes[i];
         noOfNotes[i].innerText = numberOfNotes;
 
     }
  }
+ checkButton.addEventListener("click",validateBillAndCashPaid);
