78026c9ef942568b1089f76af9cb7865488893e9
NehaBajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2022-11-06 13:02:49 +0530

    added Number to the input

--- app.js
+++ app.js
@@ -8,16 +8,17 @@ var noOfNotes = document.querySelectorAll(".no-of-notes");
 var next = document.querySelector("#next-button");
 var returnTable = document.querySelector("#return");
 var table = document.querySelector(".change-table");
-var back = document.querySelector("#back-button");
-
+ next.addEventListener("click",nextt);
+ checkButton.addEventListener("click",validateBillAndCashPaid);
 hideall()
 function hideall(){
+
     cashPaid.style.display = "none";
     checkButton.style.display = "none";
     cashLabel.style.display = "none";
     returnTable.style.display = "none";
     table.style.display = "none";
-    back.style.display = "none";
+   
 }
 
 function nextt(){
@@ -38,57 +39,39 @@ else if(bill.value <= 0){
 
 var availableNotes = [2000,500,100,20,10,5,1];
 function validateBillAndCashPaid(){
-  var bv = bill.value;
-    var cv = cashPaid.value;
-   console.log(cv);
-   console.log(bv);
-    if(cv > 0  || cv === "")
-    {
-      
-        if(cv >= bv)
-        {
-               if(cv > bv ){
+    if(cashPaid.value > 0 )
+    { 
+      if(Number(cashPaid.value) >= Number(bill.value))  {
+        if(Number(cashPaid.value) > Number(bill.value)){
             errorMsg.style.display = "block";
             returnTable.style.display = "block";
             table.style.display = "block";
-            var amountToBeReturned = cv - bv;
+            var amountToBeReturned = Number(cashPaid.value) - Number(bill.value);
             showMsg(`The amount to be returned is ${amountToBeReturned}`);
             calculateChange(amountToBeReturned);
-            console.log("dssd");
+
                }
                else{
                 errorMsg.style.display = "block";
                 showMsg("No change is to be given.");
-                returnTable.style.display = "none";
-                table.style.display = "none";
-               }
-
+                // returnTable.style.display = "none";
+                // table.style.display = "none";   
+            }
         }
-      else
-        {
-            try{
-                   noOfNotes[i]<0;
-                }
-                catch(error)
-                {
-                console.log("rtfyg");
-                }
-                finally{
-                    showMsg("The amount should be atleast equal to the bill amount");
-                    table.style.display = "none";
-                }
-         showMsg("The amount should be atleast equal to the bill amount");
-         table.style.display = "none";
+        
+        else
+        {  
+             showMsg("The amount should be atleast equal to the bill amount");
+        returnTable.style.display = "none";
+        table.style.display = "none";
         }
-    
-    
-
 }
-    else{
+
+ else{
         showMsg("You need to pay the bill!!");
     }
 }
-;
+
 function showMsg(message){
     
     errorMsg.style.display = "block";
@@ -101,23 +84,10 @@ function showMsg(message){
     {
         var numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
         amountToBeReturned=amountToBeReturned % availableNotes[i];
-      
-        noOfNotes[i].innerText = numberOfNotes;
-        // try{
-        //    noOfNotes[i]<0;
-        // }
-        // catch(error)
-        // {
-        // console.log("rtfyg");
-        // }
-        // finally{
-        //     showMsg("The amount should be atleast equal to the bill amount");
-        //     table.style.display = "none";
-        // }
-
+        noOfNotes[i].innerText = numberOfNotes;        
     }
  }
- checkButton.addEventListener("click",validateBillAndCashPaid);
- next.addEventListener("click",nextt);
+ 
+ 
 
  
